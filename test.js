b2b05dbf3dcf3650660f348dc13a7c703d1c653c
Peter Wielander <peter.wielander@contentful.com>
2021-02-19 14:45:34 +0100

    wip

--- test.js
+++ test.js
@@ -3,27 +3,16 @@ const Player = require("./Player");
 const state = {
   round: 0,
   bet_index: 0,
-  small_blind: 10,
   current_buy_in: 320,
   pot: 400,
   minimum_raise: 240,
-  dealer: 1,
-  orbits: 7,
   in_action: 1,
   players: [
     {
-      id: 0,
-      name: "Albert",
-      status: "active",
-      version: "Default random player",
       stack: 1010,
       bet: 320,
     },
     {
-      id: 1,
-      name: "Bob",
-      status: "active",
-      version: "Default random player",
       stack: 1590,
       bet: 80,
       hole_cards: [
@@ -38,10 +27,6 @@ const state = {
       ],
     },
     {
-      id: 2,
-      name: "Chuck",
-      status: "out",
-      version: "Default random player",
       stack: 0,
       bet: 0,
     },
