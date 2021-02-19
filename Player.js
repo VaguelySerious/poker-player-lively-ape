2812c38bad9961b20d01c672895b274a96fccfec
Peter Wielander <peter.wielander@contentful.com>
2021-02-19 10:31:47 +0100

    wip

--- Player.js
+++ Player.js
@@ -1,14 +1,41 @@
+const PokerRank = require("poker-rank");
+const getScore = require("scoring");
+
 class Player {
   static get VERSION() {
-    return '0.1'
+    return "0.1";
   }
 
-  static betRequest(gameState, bet) {
-    console.log('janko was here')
-    bet(0)
+  static betRequest(gs, bet) {
+    const us = gs.players[in_action];
+    const ourCards = us.hole_cards;
+    const commCards = gs.community_cards;
+
+    const callAmount = gs.current_buy_in - us[bet] + gs.minimum_raise;
+    const highestScore = getScore(ourCards, commCards);
+
+    if (highestScore < 10) {
+      const otherPlayers = gs.players((p) => p != us);
+      const highestOtherStack = otherPlayers
+        .map((o) => o.stack)
+        .reduce((acc, a) => acc + a, 0);
+
+      const ourStack = us.stack;
+
+      if (ourStack >= highestOtherStack) {
+        bet(highestOtherStack);
+      } else {
+        bet(ourStack);
+      }
+    } else if (round < 4 && highestScore < 19) {
+      // Think about raising later
+      bet(callAmount);
+    } else {
+      bet(0);
+    }
   }
 
   static showdown(gameState) {}
 }
 
-module.exports = Player
+module.exports = Player;
---
+++ scoring.js
@@ -0,0 +1,30 @@
+const suits = {
+  h: "hearts",
+  s: "spades",
+  c: "clubs",
+};
+
+function convertToPR(cards) {
+  return cards.map((c) => {
+    return {
+      rank: c.value,
+      suit: suits[c.suit],
+    };
+  });
+}
+
+function getScore(ours, comm) {
+  try {
+    const rawScores = PokerRank.scoreHands(
+      convertToPR(ours),
+      convertToPR(comm)
+    );
+    const highestScore = rawScores.map((rawScore) =>
+      PokerRank.getBestHand(rawScore)
+    );
+  } catch {
+    return 20;
+  }
+}
+
+module.exports = getScore;
